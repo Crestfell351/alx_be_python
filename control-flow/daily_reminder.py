dff4a3b0c9ca79145696c7babce0ccec046df913
Aphiwe Makhathini <116758736+Crestfell351@users.noreply.github.com>
2024-06-26 10:49:37 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,17 +1,26 @@
-task = input("Enter your task:")
-priority = input("Priority (high/medium/low):")
-time_bound =input("Is it time-bound? (yes/no):")
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+
+reminder = f"Reminder: '{task}' is a "
 
 match priority:
     case "high":
-        print(f"Reminder: {task} is a high priority task ", end="")
+        reminder += "high priority task"
         if time_bound == "yes":
-            print(f"that requires immediate attention today!")
+                reminder += " that requires immediate attention today!"
+        else:
+            reminder += " that needs to be done soon."
     case "medium":
-        print(f"Note: {task} is a medium priority task ", end="")
+        reminder += "medium priority task"
         if time_bound == "yes":
-            print(f"that requires immediate attention today!")
+            reminder += " that should be addressed today."
+        else:
+            reminder += " that you should complete soon."
     case "low":
-        print(f"Note: {task} is a low priority task ", end="")
+        reminder += "low priority task"
         if time_bound == "yes":
-            print(f"that requires immediate attention today!")
+            reminder += " but you have time to complete it."
+        else:
+            reminder += ". Consider completing it when you have free time."
+print(reminder)

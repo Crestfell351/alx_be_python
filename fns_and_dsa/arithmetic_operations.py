b4a2e5930dabcf592bc5a6adb62183db42958293
Aphiwe Makhathini <116758736+Crestfell351@users.noreply.github.com>
2024-07-05 19:44:03 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -13,6 +13,6 @@ def perform_operation(num1, num2, operation):
     case'/':
         if num2 == 0:
             print("Cannot divide by zero.")
-        else:
+        elif num2 != 0:
             res = num1 / num2
   return res

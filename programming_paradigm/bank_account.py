2094204d3b8f3f30dfc9eafb66a8b84e90cae950
Aphiwe Makhathini <116758736+Crestfell351@users.noreply.github.com>
2024-07-14 20:55:36 +0200

    Create bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class bank_account:
+    def __init__(self, account_balance):
+        self.account_balance = 0\
+        
+    def deposit(self,amount):
+        self.account_balance += amount
+
+    def withdraw(self,amount):
+        if self.account_balance>=amount:
+            self.account_balance -= amount
+            return True
+        return False
+    
+    def display_balance(self,amount):
+        print(f"This account has {self.account_balance} on it.")
+        

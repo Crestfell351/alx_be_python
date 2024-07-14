f76148f929458d3901f8db33d8b8f63b5f35accb
Aphiwe Makhathini <116758736+Crestfell351@users.noreply.github.com>
2024-07-14 21:10:46 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,12 +1,12 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.account_balance = 0\
+        self.account_balance = 0
         
     def deposit(self,amount):
         self.account_balance += amount
 
     def withdraw(self,amount):
-        if amount>0:
+        if self.account_balance>=amount:
             self.account_balance -= amount
             return True
         else:

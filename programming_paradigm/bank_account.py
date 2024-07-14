8246c71f2a5c40cf0f3873d51a6993c69df20545
Aphiwe Makhathini <116758736+Crestfell351@users.noreply.github.com>
2024-07-14 21:26:21 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,11 +6,11 @@ class BankAccount:
         self.account_balance += amount
 
     def withdraw(self,amount):
-        if self.account_balance >= amount:
+        if self.account_balance > amount:
             self.account_balance -= amount
             return True
         return False
     
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance :.2f}")
         

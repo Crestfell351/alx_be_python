53702cf50fc4a48a4b8139181b25980ec92407f6
Aphiwe Makhathini <116758736+Crestfell351@users.noreply.github.com>
2024-07-21 22:43:27 +0200

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -11,6 +11,5 @@ class Book:
     def __repr__(self):
 
         return f"Book('{self.title}', '{self.author}', {self.year})"
-        
     def __del__(self):
-        self.file.close()
+        print(f"Deleting {self.title}")
